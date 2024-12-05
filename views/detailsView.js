da41f9d6548d8b8129cd66fe9c9175ddcb07bf13
Nikolay Boev <nboev@bul-si.bg>
2024-12-05 20:04:43 +0200

    fix

--- views/detailsView.js
+++ views/detailsView.js
@@ -1,7 +1,8 @@
 import { render , html } from "../lib/lit-html.js";
 import { getOne } from "../api/itemsApi.js";
+import { getUserData } from "../utils/userUtils.js";
 
-const template = (item) => html`
+const template = (item, isOwner) => html`
   <section id="details">
           <div id="details-wrapper">
             <div>
@@ -13,19 +14,31 @@ const template = (item) => html`
                 <p class="details-price">Price: €${item.price}</p>
                 <p class="details-availability"> ${item.availability}</p>
                 <p class="type">${item.type}</p>
-                <p id="item-description">${item.desription}</p>
+                <p id="item-description">${item.description}</p>
               </div>
+              ${isOwner
+          ? html`
               <div id="action-buttons">
-                <a href="" id="edit-btn">Edit</a>
-                <a href="" id="delete-btn">Delete</a>
-              </div>
-            </div>
-          </div>
-        </section>
+                <a href="/edit/${item._id}" id="edit-btn">Edit</a>
+                <a href="/delete/${item._id}" id="delete-btn">Delete</a>
+              </div>`
+          : ''}
+           </div>
+        </div>
+  </section>
 `;
 
 export default async function detailsView(ctx) {
+    console.log('detailsView function called');
     const itemId = ctx.params.itemId;
     const item = await getOne(itemId);
-    render(template(item));
+
+    const userData = getUserData();
+    const isOwner = userData._id === item.ownerId;
+
+    console.log('User ID:', userData._id, typeof userData._id);
+    console.log('Owner ID:', item.ownerId, typeof item.ownerId);
+    
+
+    render(template(item, isOwner));
 }
